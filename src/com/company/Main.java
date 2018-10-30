5726d2b906c85574287001c1a10752a944efd0f8
kwu8742 <35508178+kwu8742@users.noreply.github.com>
2018-10-30 14:35:46 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,10 @@ public class Main {
 	    Scanner input = new Scanner(System.in);
 	    System.out.print("What do you want n to be?");
 	    int n = input.nextInt();
-	    while("
+	    int sum = 0;
+	    for(int r = 0; r <= n; r++) {
+		    sum = sum + r
+	    }
+	    System.out.println("The sum from 1 to n is" + sum);
     }
 }
