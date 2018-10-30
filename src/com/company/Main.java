49a5cadc98c35a684ffc3009d4eddd4f4893f95d
kwu8742 <35508178+kwu8742@users.noreply.github.com>
2018-10-30 14:25:36 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Scanner input = new Scanner(System.in);
+	    System.out.print("What do you want n to be?");
+	    int n = input.nextInt();
+	    while("
     }
 }
